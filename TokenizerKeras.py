d7888b2294b9dcf29013ecf59c32f8b549f4ba99
gaurav0535 <gaurav0535@hotmail.com>
2020-05-06 18:50:44 +0530

    First

--- TokenizerKeras.py
+++ TokenizerKeras.py
@@ -1,14 +1,31 @@
 from keras.preprocessing.text import Tokenizer
 
-samples = ["the cat sat on the mat","The dog ate my homework"]
+samples = ["the cat sat on the mat",
+           "The dog ate my homework",
+           "My name is gaurav srivastava and i live Noida fslfna"]
 
 
-tokenizer = Tokenizer(num_words=100)
+tokenizer = Tokenizer(num_words=100,oov_token="Checkit")
 
 tokenizer.fit_on_texts(samples)
 
-print(tokenizer.texts_to_sequences(samples))
+#print(tokenizer.texts_to_sequences(samples))
 
-print(tokenizer.word_index)
+test = ["My name is Gaurav",
+        "My name is fdalbfal kvbkk"]
 
-print(tokenizer.texts_to_matrix(samples))
+#print(tokenizer.word_index)
+
+#print(tokenizer.texts_to_sequences(test))
+
+################ Padding ##################
+
+from keras.preprocessing.sequence import pad_sequences
+
+padded = pad_sequences(tokenizer.texts_to_sequences(test),padding="post",maxlen=20)
+print(padded)
+
+
+padded = pad_sequences(tokenizer.texts_to_sequences(test),maxlen=20)
+
+print(padded)
